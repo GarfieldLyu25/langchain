5fd78adf1a90a7c09eb6c2e869d01e71a9a627f6
Garfield Lyu <lv2895045412@gmail.com>
2025-10-29 20:31:23 +0800

    Initial commit

---
+++ chapter01-summary/Test.py
@@ -0,0 +1,10 @@
+import sys
+
+import langchain
+import openai
+
+print(langchain.__version__)  #0.3.27
+
+print(openai.__version__)   #2.6.1
+
+print(sys.version) #3.9.21 | packaged by conda-forge | (main, Dec  5 2024, 13:41:22) [MSC v.1929 64 bit (AMD64)]
